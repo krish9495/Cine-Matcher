ddd8261b5da49e4cb511b1dcd4e97f116b527edf
krish9495 <krishjain786786@gmail.com>
2025-03-29 19:13:45 +0530

    Update: Add error handling for missing pickle files

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 import requests
+import os
 
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
@@ -27,29 +28,38 @@ def recommend(movie):
 
     return recommended_movies, recommended_movie_posters
 
-# Load data
-movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
-movies = pd.DataFrame(movies_dict)
-similarity = pickle.load(open('similarity.pkl', 'rb'))
-
 # Set up the Streamlit page
 st.set_page_config(page_title="Movie Recommender System", layout="wide")
 st.title('Movie Recommender System')
 
-# Create a dropdown to select a movie
-selected_movie = st.selectbox(
-    'Select a movie you like',
-    movies['title'].values
-)
+# Check if pickle files exist
+if not (os.path.exists('movie_dict.pkl') and os.path.exists('similarity.pkl')):
+    st.error("""
+    Required pickle files are missing. Please follow these steps:
+    1. Run the Jupyter notebook `Movie-recommeder-system.ipynb` to generate the pickle files
+    2. Make sure `movie_dict.pkl` and `similarity.pkl` are in the same directory as this app
+    3. Restart the app
+    """)
+else:
+    # Load data
+    movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+    movies = pd.DataFrame(movies_dict)
+    similarity = pickle.load(open('similarity.pkl', 'rb'))
 
-# Add a button to get recommendations
-if st.button('Get Recommendations'):
-    names, posters = recommend(selected_movie)
-    
-    # Display the recommendations in 5 columns
-    cols = st.columns(5)
-    
-    for idx, (col, name, poster) in enumerate(zip(cols, names, posters)):
-        with col:
-            st.text(name)
-            st.image(poster) 
+    # Create a dropdown to select a movie
+    selected_movie = st.selectbox(
+        'Select a movie you like',
+        movies['title'].values
+    )
+
+    # Add a button to get recommendations
+    if st.button('Get Recommendations'):
+        names, posters = recommend(selected_movie)
+        
+        # Display the recommendations in 5 columns
+        cols = st.columns(5)
+        
+        for idx, (col, name, poster) in enumerate(zip(cols, names, posters)):
+            with col:
+                st.text(name)
+                st.image(poster) 
